1776af15ff47bc07c498e1d83b90f0ae5653c1d5
Luciano Vilela Dourado <luciano.dourado@mpt.mp.br>
2022-03-24 11:11:08 -0300

    WIP

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
 
 const fs = require('fs');
-const debug=false;
-const log = (obj)=>{
-    if(debug)
+let debug = false;
+const log = (obj) => {
+    if (debug)
         console.log("[env_from_json]", obj);
 }
 
-const parserString = (str)=>{
+const parserString = (str) => {
     return str.replace(/[^a-zA-Z0-9_]/g, '_').trim();
 }
 
-const parserObject = (prefix, data, preferences={uppercase:true})=>{
+const parserObject = (prefix, data, preferences = { uppercase: true }) => {
     let result = {};
-    for(let key in data){
-        if(data[key] instanceof Object){
-            parserObject(`${prefix?prefix+"_":""}${key}`, data[key], preferences);
-        }else if(data[key] instanceof Array){
-            for(let i in data[key]){
-                parserObject(`${prefix?prefix+"_":""}${i}`, data[key], preferences);
+    for (let key in data) {
+        if (data[key] instanceof Object) {
+            parserObject(`${prefix ? prefix + "_" : ""}${key}`, data[key], preferences);
+        } else if (data[key] instanceof Array) {
+            for (let i in data[key]) {
+                parserObject(`${prefix ? prefix + "_" : ""}${i}`, data[key], preferences);
             }
         }
-        else{
-            let keyName = parserString(preferences.uppercase?`${prefix?prefix+"_":""}${key}`.toUpperCase():`${prefix?prefix+"_":""}${key}`);
+        else {
+            let keyName = parserString(preferences.uppercase ? `${prefix ? prefix + "_" : ""}${key}`.toUpperCase() : `${prefix ? prefix + "_" : ""}${key}`);
             log(`${keyName}=${data[key]}`);
             process.env[keyName] = data[key];
         }
     }
 
 }
-
-const config = (obj, preferences={uppercase:true, debug:false})=>{
-    let data ={};
+const init = { uppercase: true, debug: false };
+const config = (obj = undefined, preferences = { ...init }) => {
+    debug = preferences.debug;
+    let data = undefined;
     log(obj);
-    debug=preferences.debug;
-    if(obj instanceof Object){
+    if(!obj && fs.existsSync('./.env_from.json')) {
+        data=require('./.env_from.json');
+    }
+    else if (obj instanceof Object) {
         data = obj;
     }
-    else{
-      if(!fs.existsSync(obj))
-        throw `File not found: ${obj}`;
+    else if (!fs.existsSync(obj)){
+            log(`File not found: ${obj}`);
+    }else{
         data = require(obj);
     }
 
     log(data);
-    parserObject(undefined, data, preferences);
+    if(data)
+        parserObject(undefined, data, { ...preferences, ...init });
 
 }
 
-module.exports= { config };
+module.exports = { config };
 
--- test.js
+++ test.js
@@ -1,3 +1,7 @@
-require('./index').config('./test.json');
+const { config  }= require('./index');
+
+config('./test.json', {debug:true});
+config('./test.json', {uppercase:false});
+config(undefined, {debug:true});
 
 console.log(process.env);
