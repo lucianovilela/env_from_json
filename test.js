183c15a7b75111910e703568d3c97e28bc11f04b
Luciano Vilela <lucianovilela@gmail.com>
2023-08-16 11:02:18 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,35 @@
-const { config } = require('./index');
-test('chamando com debug', () => {
-    config('./test.json', { debug: true });
-});
-test('chamando com uppercase', () => {
+const { config } = require('./config'); // assuming the provided code is in a file named 'config.js'
+const assert = require('assert');
 
-    config('./test.json', { uppercase: false });
-});
-test('chamando com default', () => {
+describe('config', () => {
+    it('should create environment variables from an object', () => {
+        const data = {
+            key1: 'value1',
+            key2: {
+                key3: 'value3',
+                key4: ['value4', 'value5']
+            }
+        };
+        config(data);
+        assert.strictEqual(process.env.KEY1, 'value1');
+        assert.strictEqual(process.env.KEY2_KEY3, 'value3');
+        assert.strictEqual(process.env.KEY2_KEY4_0, 'value4');
+        assert.strictEqual(process.env.KEY2_KEY4_1, 'value5');
+    });
 
-    config(undefined, { debug: true });
+    it('should create environment variables from a file', () => {
+        const filePath = './test.json';
+        fs.writeFileSync(filePath, JSON.stringify({
+            key1: 'value1',
+            key2: {
+                key3: 'value3',
+                key4: ['value4', 'value5']
+            }
+        }));
+        config(filePath);
+        assert.strictEqual(process.env.KEY1, 'value1');
+        assert.strictEqual(process.env.KEY2_KEY3, 'value3');
+        assert.strictEqual(process.env.KEY2_KEY4_0, 'value4');
+        assert.strictEqual(process.env.KEY2_KEY4_1, 'value5');
+    });
 });
-
